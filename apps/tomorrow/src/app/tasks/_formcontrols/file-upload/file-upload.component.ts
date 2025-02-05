093ab4dac16d6117ce3506c9d3d02efb13604368
Andrew Kemp <andrew@ankemp.com>
2025-02-05 13:36:29 -0600

    Added file-upload form component

---
+++ apps/tomorrow/src/app/tasks/_formcontrols/file-upload/file-upload.component.ts
@@ -0,0 +1,71 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  effect,
+  forwardRef,
+  input,
+  model,
+  signal,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { TuiFiles } from '@taiga-ui/kit';
+
+@Component({
+  selector: 'tw-file-upload',
+  imports: [CommonModule, FormsModule, TuiFiles],
+  templateUrl: './file-upload.component.html',
+  styleUrl: './file-upload.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FileUploadComponent),
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FileUploadComponent implements ControlValueAccessor {
+  accept = input('*/*');
+  maxFileSize = input();
+  files = model<File[]>([]);
+
+  hasFiles = computed(() => {
+    return this.files().length > 0;
+  });
+
+  disabled = signal(false);
+
+  constructor() {
+    effect(() => {
+      this._onChange(this.files());
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private _onChange = (_: any) => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private _onTouched = () => {};
+
+  writeValue(input: File[]): void {
+    this.files.set(input);
+  }
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  onRemove(file: File) {
+    this.files.update((files) => files.filter((f) => f !== file));
+  }
+}
