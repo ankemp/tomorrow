3fa8c9b1d835bcf381b0440c28637f45ffc47b9a
Andrew Kemp <andrew@ankemp.com>
2025-02-08 10:29:20 -0600

    Support icon in task-list-header

--- apps/tomorrow/src/app/tasks/_primitives/task-list-header/task-list-header.component.ts
+++ apps/tomorrow/src/app/tasks/_primitives/task-list-header/task-list-header.component.ts
@@ -5,20 +5,22 @@ import {
   computed,
   input,
 } from '@angular/core';
-import { TuiTitle } from '@taiga-ui/core';
+import { TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'tw-task-list-header',
-  imports: [CommonModule, TuiHeader, TuiTitle],
+  imports: [CommonModule, TuiHeader, TuiIcon, TuiTitle],
   templateUrl: './task-list-header.component.html',
   styleUrl: './task-list-header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskListHeaderComponent {
+  icon = input<string>();
   title = input.required<string>();
   subtitle = input<string>();
   headerSize = input<TuiHeader['size']>('m');
 
+  hasIcon = computed(() => !!this.icon());
   hasSubtitle = computed(() => !!this.subtitle());
 }
