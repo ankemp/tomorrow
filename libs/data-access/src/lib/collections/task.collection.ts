ba894319c12ea974bf33874375b70b8755d15f05
Andrew Kemp <andrew@ankemp.com>
2025-04-07 22:06:46 -0500

    Fix parsing issues

--- libs/data-access/src/lib/collections/task.collection.ts
+++ libs/data-access/src/lib/collections/task.collection.ts
@@ -464,14 +464,14 @@ syncManager.addCollection(Tasks, {
       return value ? new Date(value) : null;
     }
     if (key === 'timers') {
-      return value.map((timer: any) => ({
+      return value?.map((timer: any) => ({
         ...timer,
         start: new Date(timer.start),
         end: timer.end ? new Date(timer.end) : null,
       }));
     }
     if (key === 'subTasks') {
-      return value.map((task: any) => ({
+      return value?.map((task: any) => ({
         ...task,
         completedAt: task.completedAt ? new Date(task.completedAt) : null,
       }));
