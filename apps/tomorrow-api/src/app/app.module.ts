34ec60c2de930cbb318d16d5815656e4ccd048c2
ankemp <andrew@ankemp.com>
2025-04-22 14:07:36 -0500

    Setup static file serving for SPA

--- apps/tomorrow-api/src/app/app.module.ts
+++ apps/tomorrow-api/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    TasksModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
