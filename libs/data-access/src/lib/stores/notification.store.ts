14c761abd9f6cbe3cac877c4602177d8849228c2
Andrew Kemp <andrew@ankemp.com>
2025-04-30 23:24:08 -0500

    UI capable of toggling notifications on/off

--- libs/data-access/src/lib/stores/notification.store.ts
+++ libs/data-access/src/lib/stores/notification.store.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { SwPush } from '@angular/service-worker';
 import {
   getState,
@@ -13,7 +13,20 @@ import {
   withState,
 } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { from, map, pipe, startWith, switchMap, tap } from 'rxjs';
+import { TuiAlertService } from '@taiga-ui/core';
+import { TuiPushService } from '@taiga-ui/kit';
+import {
+  catchError,
+  EMPTY,
+  filter,
+  from,
+  map,
+  mergeMap,
+  pipe,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 
 import { Settings } from './setting.store';
 
@@ -36,6 +49,8 @@ export const Notifications = signalStore(
     http: inject(HttpClient),
     swPush: inject(SwPush),
     settings: inject(Settings),
+    alertService: inject(TuiAlertService),
+    pushService: inject(TuiPushService),
   })),
   withComputed((store) => ({
     swPushEnabled: toSignal(
@@ -47,7 +62,8 @@ export const Notifications = signalStore(
     incoming: toSignal(
       store.swPush.messages.pipe(
         map((message) => {
-          return message;
+          // TODO: Create type for the message
+          return message as any;
         }),
       ),
     ),
@@ -112,6 +128,63 @@ export const Notifications = signalStore(
             }),
           );
         }),
+        catchError(() => {
+          patchState(store, { isSubscribed: false, subscription: null });
+          return store.alertService
+            .open(
+              'Notifications are not available. Please check your browser settings.',
+              {
+                label: 'Notifications Error',
+                appearance: 'negative',
+                icon: '@tui.bell-off',
+                closeable: true,
+              },
+            )
+            .pipe(mergeMap(() => EMPTY));
+        }),
+      ),
+    ),
+    unsubscribe: rxMethod<void>(
+      pipe(
+        switchMap(() => {
+          const subscription = getState(store).subscription;
+          if (!subscription) {
+            return EMPTY;
+          }
+          return store.http
+            .post('api/notifications/unsubscribe', {
+              userId: store.settings.userId(),
+              deviceId: store.settings.deviceId(),
+              subscription,
+            })
+            .pipe(
+              mergeMap(() => {
+                return subscription.unsubscribe();
+              }),
+              tap(() => {
+                patchState(store, { isSubscribed: false, subscription: null });
+              }),
+            );
+        }),
+      ),
+    ),
+    toggleSubscription() {
+      if (store.isSubscribed()) {
+        return this.unsubscribe();
+      } else {
+        return this.subscribe();
+      }
+    },
+    listenForNotifications: rxMethod<void>(
+      pipe(
+        switchMap(() => {
+          return toObservable(store.incoming).pipe(
+            filter((data) => !!data),
+            mergeMap((notification) => {
+              return store.pushService.open(notification.message);
+            }),
+          );
+        }),
       ),
     ),
   })),
@@ -119,6 +192,7 @@ export const Notifications = signalStore(
     onInit(store) {
       store.getServerPublicKey();
       store.checkSubscription();
+      store.listenForNotifications();
     },
   }),
 );
