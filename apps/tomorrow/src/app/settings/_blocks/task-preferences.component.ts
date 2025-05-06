f1720a4a2b52938392c85083d7ffcd90e6364562
ankemp <andrew@ankemp.com>
2025-05-06 17:44:57 -0500

    Preferences blocks updates

--- apps/tomorrow/src/app/settings/_blocks/task-preferences.component.ts
+++ apps/tomorrow/src/app/settings/_blocks/task-preferences.component.ts
@@ -68,143 +68,141 @@ class TimeTransformer extends TuiValueTransformer<
   ],
   template: `
     <tw-preferences-card title="Task" icon="@tui.clock">
-      <div class="switch-container">
-        <label tuiLabel>
-          Default Task Time
-          <tui-input-time
-            [ngModel]="settings.defaultReminderTime()"
-            [mode]="settings.tuiTimeFormat()"
-            [tuiTextfieldCleaner]="true"
-            (ngModelChange)="settings.updateDefaultReminderTime($event)"
-          >
-            Choose a time
-          </tui-input-time>
-        </label>
-        <label tuiLabel>
-          Time Format
-          <tui-select
-            [ngModel]="settings.timeFormat()"
-            (ngModelChange)="settings.updateTimeFormat($event)"
-          >
-            Select format
-            <tui-data-list-wrapper *tuiDataList [items]="['12h', '24h']" />
-          </tui-select>
-        </label>
-        <label tuiLabel>
-          Default Time Until Due (minutes)
-          <tui-input-slider
-            [ngModel]="settings.defaultReminderTimeAfterCreation()"
-            (ngModelChange)="
-              settings.updateDefaultReminderTimeAfterCreation($event)
-            "
-            [min]="0"
-            [max]="1440"
-            [steps]="100"
-            [segments]="12"
-            [valueContent]="durationLabel"
-          >
-            Minutes Until Due
-          </tui-input-slider>
-        </label>
-        <label tuiLabel>
-          Start of Week
-          <tui-select
-            [ngModel]="settings.startOfWeek()"
-            (ngModelChange)="settings.updateStartOfWeek($event)"
-          >
-            Select Day
-            <tui-data-list-wrapper
-              *tuiDataList
-              [items]="[
-                'Sunday',
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-              ]"
+      <label tuiLabel>
+        Default Task Time
+        <tui-input-time
+          [ngModel]="settings.defaultReminderTime()"
+          [mode]="settings.tuiTimeFormat()"
+          [tuiTextfieldCleaner]="true"
+          (ngModelChange)="settings.updateDefaultReminderTime($event)"
+        >
+          Choose a time
+        </tui-input-time>
+      </label>
+      <label tuiLabel>
+        Time Format
+        <tui-select
+          [ngModel]="settings.timeFormat()"
+          (ngModelChange)="settings.updateTimeFormat($event)"
+        >
+          Select format
+          <tui-data-list-wrapper *tuiDataList [items]="['12h', '24h']" />
+        </tui-select>
+      </label>
+      <label tuiLabel>
+        Default Time Until Due (minutes)
+        <tui-input-slider
+          [ngModel]="settings.defaultReminderTimeAfterCreation()"
+          (ngModelChange)="
+            settings.updateDefaultReminderTimeAfterCreation($event)
+          "
+          [min]="0"
+          [max]="1440"
+          [steps]="100"
+          [segments]="12"
+          [valueContent]="durationLabel"
+        >
+          Minutes Until Due
+        </tui-input-slider>
+      </label>
+      <label tuiLabel>
+        Start of Week
+        <tui-select
+          [ngModel]="settings.startOfWeek()"
+          (ngModelChange)="settings.updateStartOfWeek($event)"
+        >
+          Select Day
+          <tui-data-list-wrapper
+            *tuiDataList
+            [items]="[
+              'Sunday',
+              'Monday',
+              'Tuesday',
+              'Wednesday',
+              'Thursday',
+              'Friday',
+              'Saturday',
+            ]"
+          />
+        </tui-select>
+      </label>
+      <label tuiLabel>
+        Default Task Category
+        <tui-select
+          [ngModel]="settings.defaultReminderCategory()"
+          (ngModelChange)="settings.updateDefaultReminderCategory($event)"
+          [valueContent]="reminderSelectTemplate"
+        >
+          Select Category
+          <tui-data-list-wrapper
+            *tuiDataList
+            [items]="[null, 'Work', 'Personal', 'Shopping', 'Health']"
+            [itemContent]="reminderValueTemplate"
+          />
+          <ng-template #reminderSelectTemplate let-item>
+            <ng-container
+              [ngTemplateOutlet]="reminderChipTemplate"
+              [ngTemplateOutletContext]="{ $implicit: item, size: 'xxs' }"
             />
-          </tui-select>
-        </label>
-        <label tuiLabel>
-          Default Task Category
-          <tui-select
-            [ngModel]="settings.defaultReminderCategory()"
-            (ngModelChange)="settings.updateDefaultReminderCategory($event)"
-            [valueContent]="reminderSelectTemplate"
-          >
-            Select Category
-            <tui-data-list-wrapper
-              *tuiDataList
-              [items]="[null, 'Work', 'Personal', 'Shopping', 'Health']"
-              [itemContent]="reminderValueTemplate"
+          </ng-template>
+          <ng-template #reminderValueTemplate let-item>
+            <ng-container
+              [ngTemplateOutlet]="reminderChipTemplate"
+              [ngTemplateOutletContext]="{ $implicit: item, size: 'xs' }"
             />
-            <ng-template #reminderSelectTemplate let-item>
-              <ng-container
-                [ngTemplateOutlet]="reminderChipTemplate"
-                [ngTemplateOutletContext]="{ $implicit: item, size: 'xxs' }"
-              />
-            </ng-template>
-            <ng-template #reminderValueTemplate let-item>
-              <ng-container
-                [ngTemplateOutlet]="reminderChipTemplate"
-                [ngTemplateOutletContext]="{ $implicit: item, size: 'xs' }"
-              />
-            </ng-template>
-            <ng-template #reminderChipTemplate let-item let-size="size">
-              @if (item) {
-                <tui-chip
-                  [size]="size"
-                  appearance="custom"
-                  [style.background-color]="item | tuiAutoColor"
-                >
-                  {{ item }}
-                </tui-chip>
-              } @else if (item === null) {
-                No Default
-              }
-            </ng-template>
-          </tui-select>
-        </label>
-        <label tuiLabel>
-          Time Specificity
-          <tui-select
-            [ngModel]="settings.timeSpecificity()"
-            (ngModelChange)="settings.updateTimeSpecificity($event)"
-            [valueContent]="timeSpecificityValueTemplate"
-          >
-            Select Option
-            <!-- TODO: add optional as an item, requires writing a custom validator for our date-picker -->
-            <tui-data-list-wrapper
-              *tuiDataList
-              [items]="['always', 'never']"
-              [itemContent]="timeSpecificityValueTemplate"
-            />
-            <ng-template #timeSpecificityValueTemplate let-item>
-              <span style="text-transform: capitalize;">{{ item }}</span>
-            </ng-template>
-          </tui-select>
-        </label>
-        <label tuiLabel>
-          Complete Tasks When Subtasks are Done
-          <tui-select
-            [ngModel]="settings.autoCompleteTasks()"
-            (ngModelChange)="settings.updateAutoCompleteTasks($event)"
-            [valueContent]="autoCompleteTasksValueTemplate"
-          >
-            Select Option
-            <tui-data-list-wrapper
-              *tuiDataList
-              [items]="['always', 'never', 'ask']"
-              [itemContent]="autoCompleteTasksValueTemplate"
-            />
-            <ng-template #autoCompleteTasksValueTemplate let-item>
-              <span style="text-transform: capitalize;">{{ item }}</span>
-            </ng-template>
-          </tui-select>
-        </label>
-      </div>
+          </ng-template>
+          <ng-template #reminderChipTemplate let-item let-size="size">
+            @if (item) {
+              <tui-chip
+                [size]="size"
+                appearance="custom"
+                [style.background-color]="item | tuiAutoColor"
+              >
+                {{ item }}
+              </tui-chip>
+            } @else if (item === null) {
+              No Default
+            }
+          </ng-template>
+        </tui-select>
+      </label>
+      <label tuiLabel>
+        Time Specificity
+        <tui-select
+          [ngModel]="settings.timeSpecificity()"
+          (ngModelChange)="settings.updateTimeSpecificity($event)"
+          [valueContent]="timeSpecificityValueTemplate"
+        >
+          Select Option
+          <!-- TODO: add optional as an item, requires writing a custom validator for our date-picker -->
+          <tui-data-list-wrapper
+            *tuiDataList
+            [items]="['always', 'never']"
+            [itemContent]="timeSpecificityValueTemplate"
+          />
+          <ng-template #timeSpecificityValueTemplate let-item>
+            <span style="text-transform: capitalize;">{{ item }}</span>
+          </ng-template>
+        </tui-select>
+      </label>
+      <label tuiLabel>
+        Complete Tasks When Subtasks are Done
+        <tui-select
+          [ngModel]="settings.autoCompleteTasks()"
+          (ngModelChange)="settings.updateAutoCompleteTasks($event)"
+          [valueContent]="autoCompleteTasksValueTemplate"
+        >
+          Select Option
+          <tui-data-list-wrapper
+            *tuiDataList
+            [items]="['always', 'never', 'ask']"
+            [itemContent]="autoCompleteTasksValueTemplate"
+          />
+          <ng-template #autoCompleteTasksValueTemplate let-item>
+            <span style="text-transform: capitalize;">{{ item }}</span>
+          </ng-template>
+        </tui-select>
+      </label>
     </tw-preferences-card>
   `,
   styleUrl: './styles.css',
