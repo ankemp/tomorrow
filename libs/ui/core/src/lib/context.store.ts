09eea7f4ce9f7945f58fddce7f5ac6db47c73467
ankemp <andrew@ankemp.com>
2025-05-16 15:43:53 -0500

    Handle errors in heath endpoint (should still set state)

--- libs/ui/core/src/lib/context.store.ts
+++ libs/ui/core/src/lib/context.store.ts
@@ -14,6 +14,7 @@ import {
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { TuiAlertService, TuiBreakpointService } from '@taiga-ui/core';
 import {
+  catchError,
   EMPTY,
   fromEvent,
   iif,
@@ -159,6 +160,15 @@ export const Context = signalStore(
                         details: response.details,
                       },
                     });
+                  }),
+                  catchError((response) => {
+                    patchState(store, {
+                      apiHealth: {
+                        status: response.status,
+                        details: response.details,
+                      },
+                    });
+                    return EMPTY;
                   }),
                 );
             }),
